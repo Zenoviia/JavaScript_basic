d75fb56ec88aa737d96e5bca49c06522690173fd
Zenoviia <zenoviatkachenko@gmail.com>
2024-02-26 22:52:17 +0200

    Deleted extra comments

--- homework-03/task_08/main.js
+++ homework-03/task_08/main.js
@@ -1,16 +1,6 @@
 'use strict';
 
 
-// let num = +prompt('Введіть число:');
-
-// if (num < 20 || num > 80 ) {
-//     alert('Число не в діапазоні');
-// } else if (num != null) {
-//     alert('Число в діапазоні');
-// }
-
-
-
 let num = +prompt('Введіть число:');
 
 if (!(num < 20 || num > 80 || num != null )) {

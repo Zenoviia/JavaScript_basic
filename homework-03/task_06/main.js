3c14badca953330e5cf59b4c6b00fe811dcefc6f
Zenoviia <zenoviatkachenko@gmail.com>
2024-02-27 13:52:47 +0200

    Announced the variables before the if...else loop

--- homework-03/task_06/main.js
+++ homework-03/task_06/main.js
@@ -2,14 +2,18 @@
 
 
 let monthNumber = +prompt('Введіть номер місяця:');
+let isWinter = monthNumber >= 1 && monthNumber <= 2 || monthNumber == 12;
+let isSpring = monthNumber >= 3 && monthNumber <= 5;
+let isSummer = monthNumber >= 6 && monthNumber <= 8;
+let isAutumn = monthNumber >= 9 && monthNumber <= 11
 
-if  (monthNumber >= 1 && monthNumber <= 2 || monthNumber == 12)  {
+if  (isWinter)  {
     alert('Зима');
-} else if  (monthNumber >= 3 && monthNumber <= 5)  {
+} else if  (isSpring)  {
     alert('Весна');
-} else if  (monthNumber >= 6 && monthNumber <= 8)  {
+} else if  (isSummer)  {
     alert('Літо');
-} else if  (monthNumber >= 9 && monthNumber <= 11)  {
+} else if  (isAutumn)  {
     alert('Осінь');
 } else {
     alert('Некоректний номер місяця');

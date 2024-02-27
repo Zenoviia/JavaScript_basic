d7e2b4c8fa8887038813edd810dcbd2f2f7dfbdf
Zenoviia <zenoviatkachenko@gmail.com>
2024-02-27 14:04:00 +0200

    Removed 'num != null'

--- homework-03/task_08/main.js
+++ homework-03/task_08/main.js
@@ -3,7 +3,7 @@
 
 let num = +prompt('Введіть число:');
 
-if (!(num < 20 || num > 80 || num != null )) {
+if (!(num < 20 || num > 80 )) {
     alert('Число в діапазоні');
 } else {
     alert('Число не в діапазоні');
